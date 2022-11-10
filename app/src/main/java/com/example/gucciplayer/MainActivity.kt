7d9eea71a2fb9f1be50d4509d72fc2d853123a55
Ivan Uss <iuss@devexperts.com>
2022-11-11 01:59:07 +0300

    media player test

--- app/src/main/java/com/example/gucciplayer/MainActivity.kt
+++ app/src/main/java/com/example/gucciplayer/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.gucciplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val player by lazy { MediaPlayer.create(this, R.raw.happy_feet) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.pause).setOnClickListener {
+            player.pause()
+        }
+        findViewById<Button>(R.id.start).setOnClickListener {
+            player.start()
+        }
+        findViewById<Button>(R.id.stop).setOnClickListener {
+            player.stop()
+        }
     }
+
 }
